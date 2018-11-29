148ffe5b81bb941a8aa8cd541d067a89c8f16361
Anders Jurisoo <jurisoo@hotmail.com>
2018-11-29 11:32:17 +0800

    Added Vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,7 +7,7 @@
 
 //require('./bootstrap');
 
-//window.Vue = require('vue');
+window.Vue = require('vue');
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -15,8 +15,19 @@
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-/*Vue.component('example-component', require('./components/ExampleComponent.vue'));
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+const files = require.context('./', true, /\.vue$/i)
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key)))
+
+//Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
     el: '#app'
-});*/
+});
