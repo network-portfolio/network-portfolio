748afce77e446a7d906cb907a58979b54e857d35
Anders Jurisoo <jurisoo@hotmail.com>
2018-11-29 10:49:11 +0800

    Added tailwind and scaffold to network side

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <title>network portfolio</title>
+    <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+  </head>
+  <body class="font-sans antialiased leading-tight bg-primary2">
+    <div id="app">
+      @yield('body')
+    </div>
+    <script src="{{ mix('js/app.js') }}"></script>
+  </body>
+</html>
