1e1dd59694e9fb7916ae6a4ae4c9fd6f8ef1ee42
Anders Jurisoo <jurisoo@hotmail.com>
2018-11-29 11:32:31 +0800

    Added appheader

---
+++ resources/assets/js/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex items-center pl-4 h-8 bg-primary1">
+        <a href="https://andersjurisoo.com">
+            <div class="flex justify-center p-2 font-black rounded-t text-white">
+                network portfolio
+            </div>
+        </a>
+    </div>
+</template>
+
+<script>
+    export default {
+        //
+    }
+</script>
