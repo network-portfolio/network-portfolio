3c50c6cbf00c9c73d6142f32e90d34513ba7f0a3
Anders Jurisoo <jurisoo@hotmail.com>
2018-11-29 11:55:12 +0800

    Started styling the network site

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,12 @@
 
 @section('body')
     <app-header></app-header>
-    <div class="flex justify-center items-center max-h-full pt-8">
-        <button class="bg-red p-2 rounded hover:bg-primary3 text-white">projects</button>
+    <div class="bg-cover h-screen" v-bind:style="{ backgroundImage: 'url(https://images.unsplash.com/photo-1489710437720-ebb67ec84dd2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2fe1fcbff362a3ba7a8eed548f81430b&auto=format&fit=crop&w=1650&q=80)' }">
+        <div class="flex justify-center items-center h-full">
+            <a href="/projects">
+                <button class="bg-primary1 p-2 rounded hover:bg-primary3 text-white font-bold mr-2 p-2 px-4 text-xl">browse projects</button>
+            </a>
+            <button class="bg-transparent hover:bg-primary1 text-white font-semibold hover:text-white py-2 px-4 border border-primary1 hover:border-transparent rounded">add your own</button>
+        </div>
     </div>
 @endsection
