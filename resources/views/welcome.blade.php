bf5ab1337ebc72de876d0e59ca569ebab1a7be74
Anders Jurisoo <jurisoo@hotmail.com>
2018-12-01 07:02:56 +0800

    Experiment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('body')
     <app-header></app-header>
-    <div class="bg-cover h-screen" v-bind:style="{ backgroundImage: 'url(https://images.unsplash.com/photo-1489710437720-ebb67ec84dd2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2fe1fcbff362a3ba7a8eed548f81430b&auto=format&fit=crop&w=1650&q=80)' }">
+    <div class="bg-cover h-screen" v-bind:style="{ backgroundImage: 'url(https://images.unsplash.com/photo-1489710437720-ebb67ec84dd2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=2fe1fcbff362a3ba7a8eed548f81430b&auto=format&fit=crop&w=1650&q=80&gravity=north)' }">
         <div class="flex justify-center items-center h-full">
             <a href="/projects">
                 <button class="bg-primary1 p-2 rounded hover:bg-primary3 text-white font-bold mr-2 p-2 px-4 text-xl">browse projects</button>
