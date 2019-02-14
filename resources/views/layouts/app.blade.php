ad909146b15dc10f8a56b4f5de0dd91f95bfe1d0
Brainmaniac <olof.jondelius@gmail.com>
2019-02-14 09:11:14 +0100

    added unnecessary comment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
   </head>
   <body class="font-sans antialiased leading-tight bg-primary2">
     <script>
+        // Setting the active user
         let ACTIVE_USER_NICKNAME = "{{ Auth::user()->nickname }}";
     </script>
     <div id="app">
