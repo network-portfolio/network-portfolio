0d7a11c25eb02e3a427797ae564a34ecf04999cf
Anders Jurisoo <jurisoo@hotmail.com>
2019-02-14 16:33:20 +0800

    moved nickname check

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+    <script>
+        let ACTIVE_USER_NICKNAME = "{{ Auth::user()->nickname }}";
+    </script>
     <app-header></app-header>
     <div class="text-center">
         <edit-project project-id={{ $projectId }}></edit-project>        
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+    <script>
+        let ACTIVE_USER_NICKNAME = "{{ Auth::user()->nickname }}";
+    </script>
     <app-header></app-header>
     <div class="text-center">
         <project-list></project-list>
